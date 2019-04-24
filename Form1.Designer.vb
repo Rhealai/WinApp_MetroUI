4bb74c27d94430bfb6929be47c91aaf4612c62b0
Rhealai <44374177+Rhealai@users.noreply.github.com>
2019-04-25 02:41:12 +0800

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    'Inherits System.Windows.Forms.Form
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MetroButton1 = New MetroFramework.Controls.MetroButton()
+        Me.MetroComboBox1 = New MetroFramework.Controls.MetroComboBox()
+        Me.MetroTile1 = New MetroFramework.Controls.MetroTile()
+        Me.MetroTile2 = New MetroFramework.Controls.MetroTile()
+        Me.MetroStyleManager1 = New MetroFramework.Components.MetroStyleManager(Me.components)
+        Me.MetroRadioButton1 = New MetroFramework.Controls.MetroRadioButton()
+        Me.MetroStyleExtender1 = New MetroFramework.Components.MetroStyleExtender(Me.components)
+        CType(Me.MetroStyleManager1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MetroButton1
+        '
+        Me.MetroButton1.Location = New System.Drawing.Point(70, 88)
+        Me.MetroButton1.Name = "MetroButton1"
+        Me.MetroButton1.Size = New System.Drawing.Size(172, 53)
+        Me.MetroButton1.TabIndex = 0
+        Me.MetroButton1.Text = "MetroButton1"
+        Me.MetroButton1.UseSelectable = True
+        '
+        'MetroComboBox1
+        '
+        Me.MetroComboBox1.FormattingEnabled = True
+        Me.MetroComboBox1.ItemHeight = 21
+        Me.MetroComboBox1.Location = New System.Drawing.Point(70, 147)
+        Me.MetroComboBox1.Name = "MetroComboBox1"
+        Me.MetroComboBox1.Size = New System.Drawing.Size(284, 27)
+        Me.MetroComboBox1.TabIndex = 1
+        Me.MetroComboBox1.UseSelectable = True
+        '
+        'MetroTile1
+        '
+        Me.MetroTile1.ActiveControl = Nothing
+        Me.MetroTile1.Location = New System.Drawing.Point(70, 180)
+        Me.MetroTile1.Name = "MetroTile1"
+        Me.MetroTile1.Size = New System.Drawing.Size(192, 159)
+        Me.MetroTile1.TabIndex = 2
+        Me.MetroTile1.Text = "MetroTile1"
+        Me.MetroTile1.UseSelectable = True
+        '
+        'MetroTile2
+        '
+        Me.MetroTile2.ActiveControl = Nothing
+        Me.MetroTile2.Location = New System.Drawing.Point(286, 179)
+        Me.MetroTile2.Name = "MetroTile2"
+        Me.MetroTile2.Size = New System.Drawing.Size(187, 159)
+        Me.MetroTile2.TabIndex = 3
+        Me.MetroTile2.Text = "MetroTile2"
+        Me.MetroTile2.UseSelectable = True
+        '
+        'MetroStyleManager1
+        '
+        Me.MetroStyleManager1.Owner = Me
+        Me.MetroStyleManager1.Style = MetroFramework.MetroColorStyle.Green
+        '
+        'MetroRadioButton1
+        '
+        Me.MetroRadioButton1.AutoSize = True
+        Me.MetroRadioButton1.Location = New System.Drawing.Point(359, 45)
+        Me.MetroRadioButton1.Name = "MetroRadioButton1"
+        Me.MetroRadioButton1.Size = New System.Drawing.Size(130, 15)
+        Me.MetroRadioButton1.TabIndex = 4
+        Me.MetroRadioButton1.Text = "MetroRadioButton1"
+        Me.MetroRadioButton1.UseSelectable = True
+        '
+        'MetroStyleExtender1
+        '
+        Me.MetroStyleExtender1.Theme = MetroFramework.MetroThemeStyle.Dark
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle
+        Me.ClientSize = New System.Drawing.Size(685, 420)
+        Me.Controls.Add(Me.MetroRadioButton1)
+        Me.Controls.Add(Me.MetroTile2)
+        Me.Controls.Add(Me.MetroTile1)
+        Me.Controls.Add(Me.MetroComboBox1)
+        Me.Controls.Add(Me.MetroButton1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Theme = MetroFramework.MetroThemeStyle.[Default]
+        CType(Me.MetroStyleManager1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MetroButton1 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroComboBox1 As MetroFramework.Controls.MetroComboBox
+    Friend WithEvents MetroTile1 As MetroFramework.Controls.MetroTile
+    Friend WithEvents MetroTile2 As MetroFramework.Controls.MetroTile
+    Friend WithEvents MetroStyleManager1 As MetroFramework.Components.MetroStyleManager
+    Friend WithEvents MetroRadioButton1 As MetroFramework.Controls.MetroRadioButton
+    Friend WithEvents MetroStyleExtender1 As MetroFramework.Components.MetroStyleExtender
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub MetroButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MetroButton1.Click
+        MetroFramework.MetroMessageBox.Show(Me, "Hello World!!Metro UI!", "Notification", MessageBoxButtons.OK,
+                                            MessageBoxIcon.Information)
+    End Sub
+End Class
